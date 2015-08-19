0763e9e9c56565ae2cbe531af095d942ca1d7a20
Randall <rkb.kirby@gmail.com>
2015-08-18 23:33:57 -0500

    window stays open until closing

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,46 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 #include "Renderer.h"
 #include "Window.h"
 
+Window* w;
+Renderer* r;
+SDL_Event e;
+bool isRunning;
+
+//TODO: calculate delta time
+void GameLoop() {
+    while(isRunning) {
+        while(SDL_PollEvent(&e)) {
+            switch(e.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+            }
+        }
+
+        w->ClearScreen();
+
+        //update entity positions, do physics, etc here
+
+        //rendering here
+        r->RenderSquare(0, 0, 0.5, 0.5);
+
+        w->SwapBuffers();
+    }
+}
 
 int main(int, char**) {
     printf("hello world\n");
     printf("%d\n", CLAMP(5, 0, 1));
 
-    Window* w = new Window("test", 800, 600);
-    Renderer r = Renderer();
-    w->ClearScreen();
-    r.RenderSquare(0,0,0.5,0.75);
-    w->SwapBuffers();
+    w = new Window("test", 800, 600);
+    r = new Renderer();
+    isRunning = true;
+    GameLoop();
+
     delete w;
+    delete r;
+
     return 0;
 }
